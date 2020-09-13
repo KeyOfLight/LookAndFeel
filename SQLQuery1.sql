9972640c0b9c015584bef36997fac1a6fadc517b
KeyOfLight <70456274+KeyOfLight@users.noreply.github.com>
2020-09-12 22:38:07 -0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+Create database Usuarios
+
+use Usuarios
+
+create table Usuarios(
+
+Id int primary key identity (1,1) not null,
+Nombre varchar(40) not null,
+Contraseña varchar(40) not null,
+
+
+
+
+)
